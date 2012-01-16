7d88d7db355d4b28c6e9d927bb203991abbaed3c
Sudhir Jonathan <sudhir.j@gmail.com>
2012-01-16 19:16:50 +0530

    First version

--- app/main.py
+++ app/main.py
@@ -1,33 +1,37 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+from google.appengine.ext.ndb import context
+import jinja2
+import os
 
+debug = True
 
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+current_dir = os.path.dirname(__file__)
+current_path = os.path.abspath(current_dir)
 
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(current_dir, 'templates')))
+jinja_environment.globals.update(dict(
+    app_version = os.environ['CURRENT_VERSION_ID']
+))
 
-def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+def render(template_file, data = {}):
+    template = jinja_environment.get_template(template_file)
+    response = webapp.get_request().app.response_class()
+    response.out.write(template.render(data, url_for = webapp.uri_for))
+    return response
 
+def home(request):
+	return render('home.html')
 
-if __name__ == '__main__':
-    main()
+def faq(request):
+	return render('faq.html')
+
+ROUTES = [
+	webapp.Route('/faq', handler=faq, name='faq'),
+	webapp.Route('/', handler=home, name='home')
+]
+
+application = webapp.WSGIApplication(ROUTES, debug=debug)
+application = context.toplevel(application.__call__)
+
